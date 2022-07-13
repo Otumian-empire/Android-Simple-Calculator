297085441c6aba5c0cb2cf346a5a6dec203ed88f
otumian <popecan1000@gmail.com>
2022-07-13 12:00:23 +0000

    Add jUnit test

--- app/src/test/java/com/otumian/androidsimplecalculator/ExampleUnitTest.java
+++ app/src/test/java/com/otumian/androidsimplecalculator/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package com.otumian.androidsimplecalculator;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +11,51 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void addTwoNumbers() {
+        double resultAdd = Operation.add(1d, 1d);
+        assertEquals(resultAdd, 3d, 0.0001);
     }
+
+    @Test
+    public void addTwoNumbersNegative() {
+        double resultAdd = Operation.add(-1d, 2d);
+        assertEquals(resultAdd, 1d, 0.0001);
+    }
+
+    @Test
+    public void addTwoNumbersFloats() {
+        double resultAdd = Operation.add(1.111f, 1.111d);
+        assertEquals(resultAdd, 2.222, 0.0001);
+    }
+
+    @Test
+    public void subTwoNumbers() {
+        double resultSub = Operation.sub(1d, 1d);
+        assertEquals(resultSub, 0d, 0.0001);
+    }
+
+    @Test
+    public void subWorksWithNegativeResult() {
+        double resultSub = Operation.sub(1d, 17d);
+        assertEquals(resultSub, -16d, 0.0001);
+    }
+
+    @Test
+    public void mulTwoNumbers() {
+        double resultMul = Operation.mul(32d, 2d);
+        assertEquals(resultMul, 64d, 0.0001);
+    }
+
+    @Test
+    public void divTwoNumbers() {
+        double resultDiv = Operation.div(32d, 2d);
+        assertEquals(resultDiv, 16d, 0.0001);
+    }
+
+    @Test
+    public void divTwoNumbersZero() {
+        double resultDiv = Operation.div(32d, 0);
+        assertEquals(resultDiv, Double.POSITIVE_INFINITY, 0.0001);
+    }
+
 }
